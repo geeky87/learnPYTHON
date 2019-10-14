1abd6fe2163840a21c9a8c6476b17987aa01832c
acharya <acharya87@hotmail.com>
2019-10-14 06:23:11 +0200

    My First Commit for the Python

---
+++ Learning_Syntax/DataTypes.py
@@ -0,0 +1,32 @@
+a = 2
+print(a)
+print(type(a))
+print(id(a))
+
+b = 5642414
+print(b)
+print(type(b))
+
+c = 5414242141.34
+print(c)
+print(type(c))
+
+d = 'A'
+print(d)
+print(type(d))
+
+name = 'Acharya'
+print(name)
+print(type(name))
+
+q = True
+print(q)
+print(type(q))
+
+x = None
+print(x)
+print(type(x))
+
+import keyword
+
+print(keyword.kwlist)
