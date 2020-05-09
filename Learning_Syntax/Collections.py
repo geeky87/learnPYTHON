15ee98777c7955af992d8a6a4b9148bf8eb47888
acharya <acharya87@hotmail.com>
2020-05-09 06:51:47 +0200

    Commit for the Design Patterns

--- Learning_Syntax/Collections.py
+++ Learning_Syntax/Collections.py
@@ -1,3 +1,35 @@
+#######################################################################################################
+
+#Set Data Types
+#Sets
+basket = ('Kiran','swathi','acharya')
+print(basket)
+
+# Frozen Sets
+bucket =frozenset ('Kiran acharya')
+print(bucket)
+cities = frozenset(["Frankfurt", "Basel","Freiburg"])
+print(cities)
+
+
+#######################################################################################################
+#List Data Type
+list = ['kiran','Acharya','swathi','Prabhu']
+print(list)
+print(type(list))
+
+#######################################################################################################
+#Dictionary Data Type
+my_dic = {"name":"acharya","age":10}
+print(my_dic)
+print(my_dic['name'])
+print(my_dic.keys())
+
+#######################################################################################################
+#Tuple Data Type
+tuple = (123,'hello')
+print(tuple)
+
 from collections import Counter
 from sortedcontainers import sortedlist
 
--- Learning_Syntax/DataTypes.py
+++ Learning_Syntax/DataTypes.py
@@ -21,38 +21,7 @@ print(a_str) #output will be whole string. Hello World
 print(a_str[0]) #output will be first character. H
 print(a_str[0:5]) #output will be first five characters. Hello
 
-#######################################################################################################
 
-#Set Data Types
-#Sets
-basket = ('Kiran','swathi','acharya')
-print(basket)
-
-# Frozen Sets
-bucket =frozenset ('Kiran acharya')
-print(bucket)
-cities = frozenset(["Frankfurt", "Basel","Freiburg"])
-print(cities)
-
-
-
-#######################################################################################################
-#List Data Type
-list = ['kiran','Acharya','swathi','Prabhu']
-print(list)
-print(type(list))
-
-#######################################################################################################
-#Dictionary Data Type
-my_dic = {"name":"acharya","age":10}
-print(my_dic)
-print(my_dic['name'])
-print(my_dic.keys())
-
-#######################################################################################################
-#Tuple Data Type
-tuple = (123,'hello')
-print(tuple)
 
 
 
